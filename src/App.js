a23e7a669594c424bae00dc7807472f0cd266d21
crazyArtur <artur.dbsz@gmail.com>
2019-12-09 19:11:53 +1100

    Installed TypeScript + installed StyledComponents + added TypeScript testing folder

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          こんにちは世界！
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// var element = React.createElement('h1', {className: 'greeting'}, );
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
